aa0fc0d3f6c84829a8cb4424b605f3a9dd5548a3
Takayuki Watanabe <watanabe@sikmi.com>
2024-09-27 17:34:28 +0900

    コメント削除

--- EnvironmentSample/ContentView.swift
+++ EnvironmentSample/ContentView.swift
@@ -3,13 +3,9 @@ import SwiftUI
 /// https://developer.apple.com/documentation/swiftui/bindable
 
 struct ContentView: View {
-  //@Environment(Library.self) private var library
-//  @State private var path = NavigationPath()
-
   @Environment(PathVM.self) private var pathVM
   
   var body: some View {
-//    @Bindable var library = library
     @Bindable var pathVM = pathVM
     NavigationStack(path: $pathVM.path) {
       VStack {
