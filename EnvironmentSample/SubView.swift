997596699c2215cfce4529442c4cf4c09b4f876f
Takayuki Watanabe <watanabe@sikmi.com>
2024-10-03 14:23:09 +0900

    onAppearではなくて、Buttonを押した時にタイマーが動くようにする

--- EnvironmentSample/SubView.swift
+++ EnvironmentSample/SubView.swift
@@ -4,20 +4,25 @@ struct SubView: View {
   @State var value: String?
   @Environment(PathVM.self) private var pathVM
   @State var timer: Timer?
+  @State var buttonDisable: Bool = false
   
   var body: some View {
     VStack {
-      Text("ここは、SubViewです、3秒後にSub2Viewに移動します。")
-    }
-    .onAppear {
-      // Web APIなどで通信が完了したら画面遷移するイメージ
-      timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
-        pathVM.someVariable = "from SubView Variable"
-        pathVM.path.append(Route.sub2)
+      Text("ここは、SubViewです")
+      Button("このボタンを押すと3秒後にSub2Viewに移動します") {
+        buttonDisable = true
+        // Web APIなどで通信が完了したら画面遷移するイメージ
+        timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
+          pathVM.someVariable = "from SubView Variable"
+          pathVM.path.append(Route.sub2)
+          buttonDisable = false
+        }
       }
+      .disabled(buttonDisable)
     }
     .onDisappear {
       timer?.invalidate()
+      buttonDisable = false
     }
   }
 }
